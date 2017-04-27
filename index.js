129b8d24006957fe4b6764fdb84e6c714b0b8e31
aeternum-vale <aeternum-vale@ya.ru>
2017-04-27 17:56:03 +0300

    changed auth view

--- index.js
+++ index.js
@@ -18,7 +18,7 @@ app.get('/', function(req, res){
 });
 
 app.get('/authorization', function(req, res){
-	res.render('authorization');
+	res.render('authorization', { page: 'authorization'});
 });
 
 
