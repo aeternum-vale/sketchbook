efd131b9f70405ce65e5c1b506b8dfc54091647a
aeternum-vale <aeternum-vale@ya.ru>
2017-04-28 20:50:48 +0300

    changed the structure and brought some changes to authorization window

--- frontend/pages/authorization/script.js
+++ frontend/pages/authorization/script.js
@@ -1,3 +1,19 @@
 import './style.less';
 
-console.log("start");
+let content = document.getElementsByClassName("window__content")[0];
+
+addEventListener('focus', function(e) {
+	if (e.target.classList && e.target.classList.contains('textbox__field')) {
+		let tb = e.target.closest('.textbox');
+		if (tb)
+			tb.classList.add('textbox_focus');
+	}
+}, true);
+
+addEventListener('blur', function(e) {
+	if (e.target.classList && e.target.classList.contains('textbox__field')) {
+		let tb = e.target.closest('.textbox');
+		if (tb)
+			tb.classList.remove('textbox_focus');
+	}
+}, true);
